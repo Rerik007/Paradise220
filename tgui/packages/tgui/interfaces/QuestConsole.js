ab9de02c1caeba6c72ceb78550f87dd850c4175e
SuhEugene <32931701+SuhEugene@users.noreply.github.com>
2023-11-08 23:00:30 +0300

    Move code and remove old pane

--- tgui/packages/tgui/interfaces/QuestConsole.js
+++ tgui/packages/tgui/interfaces/QuestConsole.js
@@ -4,6 +4,24 @@ import { Button, Tabs, Box, Flex, Section, Tooltip, Icon, LabeledList, Table } f
 import { Window } from "../layouts";
 import { decodeHtmlEntities } from 'common/string';
 
+
+const getRewardColor = (reward, isCorp) => {
+  if (isCorp) { reward /= 10; }
+
+  if (reward > 1100) { return "purple"; }
+  if (reward > 500) { return "orange"; }
+  if (reward > 250) { return "yellow"; }
+  return "green";
+};
+
+const mapTwoByTwo = (a, c) => {
+  let result = [];
+  for (let i = 0; i < a.length; i += 2)
+  { result.push(c(a[i], a[i + 1], i)); }
+  return result;
+};
+
+
 export const QuestConsole = (props, context) => {
   const [tabName, setTab] = useLocalState(context, 'tabName', 'centcomm');
   return (
@@ -92,22 +110,6 @@ const StatusPane = (properties, context) => {
   );
 };
 
-const getRewardColor = (reward, isCorp) => {
-  if (isCorp) { reward /= 10; }
-
-  if (reward > 1100) { return "purple"; }
-  if (reward > 500) { return "orange"; }
-  if (reward > 250) { return "yellow"; }
-  return "green";
-};
-
-const mapTwoByTwo = (a, c) => {
-  let result = [];
-  for (let i = 0; i < a.length; i += 2)
-  { result.push(c(a[i], a[i + 1], i)); }
-  return result;
-};
-
 
 const QuestPane = (properties, context) => {
   const { data } = useBackend(context);
@@ -127,12 +129,14 @@ const QuestPane = (properties, context) => {
   );
 };
 
+
 const QuestFastTimeBonus = properties => (
   <Box position="absolute" right={0.7} top={0.6}>
     <Tooltip content="Fast time bonus active" position="left" />
     <Icon name="fire" size={1.3} color={properties.color || 'average'} />
   </Box>
 );
+
 
 const QuestItem = (properties, context) => {
   const [cardButtonsShown, setCardButtonsShown] = useLocalState(context, `cardButtonsShown`, false);
@@ -218,6 +222,7 @@ const QuestItem = (properties, context) => {
   );
 };
 
+
 const QuestItemTask =properties => {
   const { task } = properties;
   return (
@@ -242,70 +247,3 @@ const QuestItemTask =properties => {
     </Flex>
   );
 };
-
-
-
-const QuestPageOLD = (properties, context) => {
-  const { act, data } = useBackend(context);
-  const {
-    source_customer,
-  } = properties;
-  const {
-    quests,
-  } = data;
-  return (
-    <Box>
-      {quests.map((quest, index) => (
-        (source_customer !== quest.customer
-        ) || (
-          <Section
-            title={"Quest " + ++index}
-            className={"QuestConsole__Section"+quest.reward_color}
-            buttons={
-              <Box>
-                <Button
-                  content={quest.active ? <Box bold> QUEST ACTIVE</Box> : 'Activate'}
-                  disabled={quest.active}
-                  onClick={() => act('activate', { uid: quest.ref })}
-                />
-                <Button
-                  content={<Box bold>Reroll</Box>}
-                  disabled={quest.active}
-                  onClick={() => act('denied', { uid: quest.ref })}
-                />
-              </Box>
-            }
-            style={{
-              'text-align': 'center',
-              'background-color': '#4b4d4b',
-              'white-space': 'pre',
-              margin: '10px',
-              'border-radius': '15px',
-              'margin-right': '5%',
-              'margin-top': '25px',
-              'bottom-color': '#111111',
-            }}>
-            <Flex
-              style={{
-                'text-align': 'left',
-                'vertical-align': 'middle',
-                'flex-direction': 'column',
-                spacing: '1',
-              }}>
-
-              <Flex style={{
-                'vertical-align': 'middle',
-                margin: '0px',
-                'margin-left': '15px',
-                outline: '3px solid #161613',
-                'background-color': '#3e3d4a',
-              }} />
-            </Flex>
-          </Section>
-        )
-      ))}
-    </Box>
-  );
-};
-
-
